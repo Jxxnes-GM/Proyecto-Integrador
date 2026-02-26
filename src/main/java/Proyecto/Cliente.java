417da30480d69f750a0b3e1adc6bca606faf9704
Juanes <jestebangaviria@estudiante.uniajc.edu.co>
2026-02-25 21:52:55 -0500

    Ordenando carpetas

--- src/main/java/Proyecto/Cliente.java
+++ src/main/java/Proyecto/Cliente.java
@@ -1,4 +1,4 @@
-package com.example;
+package Proyecto;
 
 public class Cliente {
     
--- src/main/java/Proyecto/Main.java
+++ src/main/java/Proyecto/Main.java
@@ -1,4 +1,4 @@
-package com.example;
+package Proyecto;
 
 public class Main {
     public static void main(String[] args) {
